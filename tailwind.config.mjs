fe27275a2ed828927140c5b5780f13ffb9e96ae0
dctalbot <dave.c.talbot@gmail.com>
2025-04-09 01:40:22 -0400

    revamp

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,11 +3,7 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
-      colors: {
-        "garden-green": "#2F5233",
-        leaf: "#4F7942",
-        soil: "#5C4033",
-      },
+      colors: {},
     },
   },
 };
